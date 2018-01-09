d1a114c438184165f14026b798bb6e285483bb5c
jinhongyuan <jinhongyuan@360.net>
2018-01-09 17:16:11 +0800

    only allow unique plugin case names

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
-# UNDER CONSTRUCTION
-## Todo
-- [] Implement deep clone algorithm with higher efficiency
-- [] Support unit test
-
 ## Webpack Run Once plugin
+
 This is a webpack plugin that enables developers to run specific webpack plugins only for the first compilation under webpack's watch mode.
+
 ### Installation
+
 ```shell
 npm install run-once-plugin --save-dev
 ```
+
 ### Basic Usage
+
 ```javascript
 var RunOncePlugin = require('run-once-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin'); // third party plugin
@@ -36,13 +36,16 @@ var webpackConfig = {
                 name: 'parse-html2',
                 plugin: HtmlWebpackPlugin,
                 option: {}
-            },
-        ]),
+            }
+        ])
     ]
 };
 ```
+
 ### Configuration
+
 The plugin accepts an array of plugins and their configuration.
+
 * `name: string` works as keys for different plugins
 * `plugin: {...}` to specify the plugin you want to use
 * `option: any` to pass into the specified plugin
--- index.js
+++ index.js
@@ -1,84 +1,105 @@
 function traverseArray(prev, cur, key) {
-	let res = [];
-	for (let i = prev.length; i < cur.length; i++) {
-		res.push(cur[i]);
-	}
-	return res;
+    let res = [];
+    for (let i = prev.length; i < cur.length; i++) {
+        res.push(cur[i]);
+    }
+    return res;
 }
 
 function deepClone(obj) {
-	let res = {};
-	Object.keys(obj).forEach((item) => {
-		if (Array.isArray(obj[item])) {
-			res[item] = [];
-			obj[item].forEach((k) => res[item].push(k))
-		} else {
-			res[item] = Object.assign(obj[item])
-		}
-	})
-	return res;
+    let res = {};
+    Object.keys(obj).forEach(item => {
+        if (Array.isArray(obj[item])) {
+            res[item] = [];
+            obj[item].forEach(k => res[item].push(k));
+        } else {
+            res[item] = Object.assign(obj[item]);
+        }
+    });
+    return res;
 }
 
 class WebpackRunOncePlugin {
-	constructor(pluginList) {
-		this.isRunned = false;
-		this.pluginList = pluginList;
-		this.additionalEnv = {};
-	}
+    constructor(pluginList) {
+        this._isRunned = false;
+        let _record = {};
 
-	apply(compiler) {
-		compiler.plugin('environment', function environmentEvent(compilation, callback) {
-			//this event only execute once
-			if (!this.isRunned) {
-				this.pluginList.forEach(item => {
-					let {plugin, option} = item;
-					item.additionalEnv = {};
-					let before,
-						after;
-					//preserve compiler plugins status both before and after
-					before = deepClone(compiler._plugins);
-					compiler.apply.apply(compiler, [new plugin(option)]);
-					after = deepClone(compiler._plugins);
+        //select unique plugin cases
+        this.pluginList = pluginList.filter(function(plugin) {
+            if (_record.hasOwnProperty(plugin.name)) {
+                console.warn(
+                    'The plugin case "' +
+                        plugin.name +
+                        '" already exists and only the first one will be executed. Please use unique names for each plugin case.'
+                );
+                return false;
+            }
+            _record[plugin.name] = 1;
+            return true;
+        });
+        this.additionalEnv = {};
+    }
 
-					//record newly installed plugins
-					Object.keys(after).forEach((key) => {
-						if (!before.hasOwnProperty(key)) {
-							item.additionalEnv[key] = traverseArray([], after[key]);
-						} else {
-							item.additionalEnv[key] = traverseArray(
-								item.additionalEnv[key]
-								? item.additionalEnv[key]
-								: before[key],
-							after[key],
-							key);
-						}
-					})
-				})
-			}
-		}.bind(this))
-		compiler.plugin('after-emit', function afterCompileEvent(compilation, cb) {
-			if (!this.isRunned) {
-				this.isRunned = true;
-				let compilerPlugins = compiler._plugins;
-				//clear all the newly installed plugins after the first run
-				this.pluginList.forEach(item => {
-					let plugins = item.additionalEnv;
-					Object.keys(plugins).forEach((key) => {
-						if (plugins[key].length) {
-							let comp = compilerPlugins[key];
-							for (let fn of plugins[key]) {
-								var pos = comp.indexOf(fn);
-								if (pos != -1)
-									comp.splice(pos, 1);
-								}
-							}
-					})
-				})
-			}
-			cb();
+    apply(compiler) {
+        compiler.plugin(
+            'environment',
+            function environmentEvent(compilation, callback) {
+                //this event only execute once
+                if (!this._isRunned) {
+                    this.pluginList.forEach(item => {
+                        let { plugin, option } = item;
+                        item.additionalEnv = {};
+                        let before, after;
+                        //preserve compiler plugins status both before and after
+                        before = deepClone(compiler._plugins);
+                        compiler.apply.apply(compiler, [new plugin(option)]);
+                        after = deepClone(compiler._plugins);
 
-		}.bind(this))
-	}
+                        //record newly installed plugins
+                        Object.keys(after).forEach(key => {
+                            if (!before.hasOwnProperty(key)) {
+                                item.additionalEnv[key] = traverseArray(
+                                    [],
+                                    after[key]
+                                );
+                            } else {
+                                item.additionalEnv[key] = traverseArray(
+                                    item.additionalEnv[key]
+                                        ? item.additionalEnv[key]
+                                        : before[key],
+                                    after[key],
+                                    key
+                                );
+                            }
+                        });
+                    });
+                }
+            }.bind(this)
+        );
+        compiler.plugin(
+            'after-emit',
+            function afterCompileEvent(compilation, cb) {
+                if (!this._isRunned) {
+                    this._isRunned = true;
+                    let compilerPlugins = compiler._plugins;
+                    //clear all the newly installed plugins after the first run
+                    this.pluginList.forEach(item => {
+                        let plugins = item.additionalEnv;
+                        Object.keys(plugins).forEach(key => {
+                            if (plugins[key].length) {
+                                let comp = compilerPlugins[key];
+                                for (let fn of plugins[key]) {
+                                    var pos = comp.indexOf(fn);
+                                    if (pos != -1) comp.splice(pos, 1);
+                                }
+                            }
+                        });
+                    });
+                }
+                cb();
+            }.bind(this)
+        );
+    }
 }
 
 module.exports = WebpackRunOncePlugin;
